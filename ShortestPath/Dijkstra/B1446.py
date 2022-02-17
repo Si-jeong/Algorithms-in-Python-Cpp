4cd0096b9688e57a1310710842f3064c7555ea82
Si-jeong <ssonge413@gmail.com>
2022-02-18 00:30:44 +0900

    Add comments

--- ShortestPath/Dijkstra/B1446.py
+++ ShortestPath/Dijkstra/B1446.py
@@ -12,11 +12,10 @@ for i in range(n):
 distance = [i for i in range(d+1)]
 
 for i in range(d+1):
-    if i != 0:
+    if i != 0:  # 이전 지점에서 고속도로로 도착하는 경우
         distance[i] = min(distance[i], distance[i-1]+1)
     for w, e in shortcut[i]:
-        if e <= d and distance[e] > w + distance[i]:
+        if e <= d and distance[e] > w + distance[i]: # 시작 지점이 i인 지름길
             distance[e] = w + distance[i]
 
 print(distance[d])
-    
